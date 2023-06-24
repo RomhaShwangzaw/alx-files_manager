98c4a2aae9bf3dbc5e0571683238d1092afc5598
Romha Shwangzaw <romhas@gmail.com>
2023-06-24 19:52:32 +0300

    Added Error checking for the entire API

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 import UsersController from '../controllers/UsersController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import { APIError, errorResponse } from '../middlewares/error';
 
 /**
  * Binds the routes to the appropriate handler in the
@@ -25,6 +26,11 @@ const mapRoutes = (app) => {
   app.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
   app.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
   app.get('/files/:id/data', FilesController.getFile);
+
+  app.all('*', (req, res, next) => {
+    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
+  });
+  app.use(errorResponse);
 };
 
 export default mapRoutes;
