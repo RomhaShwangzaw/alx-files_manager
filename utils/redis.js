6ff56071b49e927aa5c4877400594751e5f1d9d3
Romha Shwangzaw <romhas@gmail.com>
2023-06-24 11:47:21 +0300

    Added helper functions for Redis operations

--- utils/redis.js
+++ utils/redis.js
@@ -1,32 +1,63 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+/**
+ * Represents a Redis client.
+ */
 class RedisClient {
+  /**
+   * Creates a new RedisClient instance.
+   */
   constructor() {
     this.client = createClient();
+    this.isClientConnected = true;
     this.client.on('error', (err) => {
       console.log('Redis client not connected to the server:',
         err.message || err.toString());
+      this.isClientConnected = false;
+    });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
   }
 
+  /**
+   * Checks if this client's connection to the Redis server is active.
+   * @returns {boolean}
+   */
   isAlive() {
-    return this.client.connected;
+    return this.isClientConnected;
   }
 
+  /**
+   * Retrieves the value of a given key.
+   * @param {String} key The key of the item to retrieve.
+   * @returns {String | Object}
+   */
   async get(key) {
     return promisify(this.client.get).bind(this.client)(key);
   }
 
+  /**
+   * Stores a key and its value along with an expiration time.
+   * @param {String} key The key of the item to store.
+   * @param {String | Number | Boolean} value The item to store.
+   * @param {Number} duration The expiration time of the item in seconds.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     promisify(this.client.setex).bind(this.client)(key, duration, value);
   }
 
+  /**
+   * Removes the value of a given key.
+   * @param {String} key The key of the item to remove.
+   * @returns {Promise<void>}
+   */
   async del(key) {
     promisify(this.client.del).bind(this.client)(key);
   }
 }
 
 const redisClient = new RedisClient();
-
 export default redisClient;
